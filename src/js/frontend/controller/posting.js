1f90ce7c2d118f00819facec8a91dbd0c1fe81b8
kkdd0757 <kkdd0757@hanmail.net>
2021-11-11 21:45:56 +0900

    feat: styling and js for posting and newstudy page
    - put drag and drop box
    
    - put slide range bar

--- src/js/frontend/controller/posting.js
+++ src/js/frontend/controller/posting.js
@@ -1,21 +1,99 @@
 // DOM Nodes
 const $fileUploader = document.querySelector('.chosen-image');
-
+const $uploadButton = document.querySelector('.upload-btn');
+const $imageTitle = document.querySelector('.image-title');
+const $uploadImg = document.querySelector('.upload-img');
+const $hashTag = document.querySelectorAll('.hash-tag');
+const splitArray = $hashTag.split(' ');
+const linkedTag = '';
 // Functions
 const uploadFile = e => {
   const reader = new FileReader();
-  const button = document.querySelector('.upload-btn');
-
   // onload는 파일이 성공적으로 load 되었을 경우 실행하는 이벤트 핸들러.
   reader.onload = event => {
-    const img = document.querySelector('.upload-img');
-    img.setAttribute('src', event.target.result);
-    console.log(event.target.result);
+    $uploadImg.setAttribute('src', event.target.result);
   };
   reader.readAsDataURL(e.target.files[0]);
-  console.log(e.target.files[0].log);
-  button.setAttribute('data-label', e.target.files[0].name);
+  $uploadButton.setAttribute('data-label', e.target.files[0].name);
+  $imageTitle.textContent = e.target.files[0].name;
 };
 
+/*
+ * drag and drop
+ *@param {HTMLElement} dropZoneElement
+ *@param {File} file
+ */
+
+function updateThumbnail(dropZoneElement, file) {
+  let $thumbnailElement = dropZoneElement.querySelector('.drop-zone__thumb');
+
+  // First time - remove the prompt
+  if (dropZoneElement.querySelector('.drop-zone__prompt')) {
+    dropZoneElement.querySelector('.drop-zone__prompt').remove();
+  }
+
+  // First time - create thumbnail element
+  if (!$thumbnailElement) {
+    $thumbnailElement = document.createElement('div');
+    $thumbnailElement.classList.add('drop-zone__thumb');
+    dropZoneElement.appendChild($thumbnailElement);
+  }
+
+  // file is proper file object
+  $thumbnailElement.dataset.label = file.name;
+
+  // show thumbnail for image files
+  if (file.type.startsWith('image/')) {
+    const reader = new FileReader();
+
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+      $thumbnailElement.style.backgroundImage = `url('${reader.result}')`;
+    };
+  } else {
+    $thumbnailElement.style.backgroundImage = null;
+  }
+}
 // Event bindings
 $fileUploader.addEventListener('input', uploadFile);
+
+// drag and drop
+document.querySelectorAll('.drop-zone__input').forEach(inputElement => {
+  const dropZoneElement = inputElement.closest('.drop-zone');
+
+  dropZoneElement.addEventListener('click', () => {
+    inputElement.click();
+  });
+
+  inputElement.addEventListener('change', () => {
+    if (inputElement.file.length) {
+      updateThumbnail(dropZoneElement, inputElement.files[0]);
+    }
+  });
+  // drag over했을때 border change됨
+  dropZoneElement.addEventListener('dragover', e => {
+    e.preventDefault();
+    dropZoneElement.classList.add('drap-zone--over');
+  });
+
+  // dragleave or dragend remove the class which changes the border style
+  ['dragleave', 'dragend'].forEach(type => {
+    dropZoneElement.addEventListener(type, () => {
+      dropZoneElement.classList.remove('drop-zone--over');
+    });
+  });
+
+  dropZoneElement.addEventListener('drop', e => {
+    e.preventDefault();
+    if (e.dataTransfer.files.length) {
+      // actual file lists(e~) signing in inputElement
+      inputElement.files = e.dataTransfer.files;
+      updateThumbnail(dropZoneElement, e.dataTransfer.files[0]);
+    }
+    // console.log(e.dataTransfer.files);
+
+    dropZoneElement.classList.remove('drop-zone--over');
+  });
+});
+
+// hash-tag
