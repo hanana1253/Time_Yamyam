a9ba806a93c4b55b21d506d6de792ef275013495
hanana1253 <hanana1253@gmail.com>
2021-11-11 21:48:52 +0900

    fix: Modulize login.js

--- src/js/frontend/controller/login.js
+++ src/js/frontend/controller/login.js
@@ -7,71 +7,60 @@ import {
   signInWithEmailAndPassword,
 } from 'firebase/auth';
 import { getFirestore } from 'firebase/firestore';
-import { loginSchema, signupSchema } from '../utils/schema.js';
-import { throttle } from '../utils/helper.js';
 import axios from 'axios';
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: 'AIzaSyAoSFp0zS691ErJuAjGbwuQZMSjEtfxgi0',
-  authDomain: 'timeyamyam-8101d.firebaseapp.com',
-  databaseURL: 'https://timeyamyam-8101d-default-rtdb.firebaseio.com',
-  projectId: 'timeyamyam-8101d',
-  storageBucket: 'timeyamyam-8101d.appspot.com',
-  messagingSenderId: '345524870175',
-  appId: '1:345524870175:web:94aef6d22c707ca693892a',
-  measurementId: 'G-1HXK10SP5N',
-};
+import { firebaseConfig } from '../utils/firebaseConfig.js';
+import { throttle } from '../utils/helper.js';
+import {
+  getCurrentForm,
+  getErrorMsgByInputName,
+  getIsValid,
+  getIsValidByInputName,
+  setCurrentForm,
+  setCurrentSchema,
+  setSchemaValueByInputName,
+} from '../store/login.js';
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth();
-const db = getFirestore();
 
 const $loginForm = document.querySelector('.login.auth-form');
 const $signupForm = document.querySelector('.signup.auth-form');
 const $loginFormSubmit = document.querySelector('.login.button');
 const $signupFormSubmit = document.querySelector('.signup.button');
 
-let currentForm = 'login';
-let schema = loginSchema;
 let $currentForm = $loginForm;
 let $currentFormSubmit = $loginFormSubmit;
 
 const toggleCurrentForm = () => {
   $currentForm.reset();
-  if (currentForm === 'login') {
-    currentForm = 'signup';
-    schema = signupSchema;
-
+  if (getCurrentForm() === 'login') {
+    setCurrentForm('signup');
     $currentForm = $signupForm;
     $currentFormSubmit = $signupFormSubmit;
   } else {
-    currentForm = 'login';
-    schema = loginSchema;
-
+    setCurrentForm('login');
     $currentForm = $loginForm;
     $currentFormSubmit = $loginFormSubmit;
   }
-
+  setCurrentSchema(getCurrentForm())
   document.querySelectorAll('.auth-form').forEach($form => $form.classList.toggle('hidden'));
 };
 
 const setErrorMsg = inputName => {
-  $currentForm.querySelector(`input[name=${inputName}] ~ .error`).textContent = schema[inputName].isValid
+  $currentForm.querySelector(`input[name=${inputName}] ~ .error`).textContent = getIsValidByInputName(inputName)
     ? ''
-    : schema[inputName].error;
+    : getErrorMsgByInputName(inputName);
 };
 
 const activateSubmitButton = () => {
-  $currentFormSubmit.disabled = !schema.isValid;
+  $currentFormSubmit.disabled = !getIsValid();
 };
 
 const validate = throttle(e => {
   const { name, value } = e.target;
 
-  schema[name].value = value.trim();
+  setSchemaValueByInputName(name, value.trim());
   setErrorMsg(name);
   activateSubmitButton();
 }, 300);
@@ -87,7 +76,7 @@ const submit = async e => {
 
   // console.log(`POST /${currentForm}`, formData);
 
-  if (currentForm === 'login') {
+  if (getCurrentForm() === 'login') {
     const { user } = await signInWithEmailAndPassword(auth, formData.email, formData.password);
   } else {
     const { user } = await createUserWithEmailAndPassword(auth, formData.email, formData.password);
