4c5e4a75d71cf64e84812aa49ae383a5d1076bcc
kkdd0757 <kkdd0757@hanmail.net>
2021-11-12 21:43:25 +0900

    feat: Add func posting and new study page
    1. Fix drag and drop image upload
    
    2. Styling has been changed in image upload part

--- src/js/frontend/controller/newstudy.js
+++ src/js/frontend/controller/newstudy.js
@@ -1,20 +1,22 @@
 const sheet = document.createElement('style');
 const $rangeInput = document.querySelector('.range-bar__input');
-const prefs = ['webkit-slider-runnable-track', 'moz-range-track', 'ms-track'];
-
-// const $slider = document.querySelector('.range-bar__input');
+const $slider = document.querySelector('.range-bar__input');
 const $newValue = document.querySelector('.new-value');
 const $rangeBar = document.querySelector('.range-bar');
 const $levelOutput = document.querySelector('.level-output');
+const $valueSpan = document.querySelector('.value-span');
+
 $rangeBar.oninput = () => {
   $levelOutput.value = $rangeInput.value;
+  $valueSpan.innerHTML = $levelOutput;
+  console.log($levelOutput.value);
 };
 // $newValue.textContent = $slider.value;
 // $slider.oninput = () => {
 //   $newValue.textContent = this.value;
 // };
-// $slider.addEventLister('mousemove', () => {
-//   const inputValue = $slider.value;
-//   const inputColor = `background-image: linear-gradient(90deg, rgb(117, 252, 117) ${inputValue}%, rgb(214, 214, 214) ${inputValue}%);`;
-//   $slider.style.background = inputColor;
-// });
+$slider.addEventLister('mousemove', () => {
+  const inputValue = $slider.value;
+  const inputColor = `background-image: linear-gradient(90deg, rgb(117, 252, 117) ${inputValue}%, rgb(214, 214, 214) ${inputValue}%);`;
+  $slider.style.background = inputColor;
+});
